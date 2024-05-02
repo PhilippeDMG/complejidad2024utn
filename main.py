d57af44f7c766294685f4f0e7b7e1d27e4e36f39
Philippe Maurel <philippedamau@gmail.com>
2024-05-02 18:22:19 -0300

    medir tiempo

--- main.py
+++ main.py
@@ -1,51 +1,18 @@
 """TP complejidad nros sociales"""
 
+import time
+
 from utils import sum_divs1 as s
-
-# def main():
-#    """encontrar perfectos hasta lim"""
-#    lim = 1000
-#    space = [True] * 3 * lim  # hasta q nro habria q guardar?
-#    for i in range(lim):
-#        sequence = [i]
-#        tries = 0
-#        holder = i
-#        while tries < 30:
-#            tries += 1
-#            res = s(holder)
-#            if res > 2 * i:
-#                print(i)
-#            if space[res]:  # nro no explorado
-#                if res in sequence:
-#                    subseq_start = sequence.index(res)
-#                    social_nbrs = sequence[subseq_start:]
-#                    for nbr in sequence:
-#                        space[nbr] = False
-#                    print(f"sequence of length {len(social_nbrs)}: {social_nbrs}")
-#                    break
-#                else:
-#                    sequence.append(res)
-#                    holder = res
-#            else:  # nro q no es social o q si es social
-#                for elem in sequence:
-#                    space[elem] = False
-#                break
-#        if tries == 30:
-#            print("30 reps")
-#            for elem in sequence:
-#                space[elem] = False
-
-# with open("output.txt", "w") as file:
-#    for i in range(lim):
-#        if perfs[i]:
-#            file.write(str(i) + "\n")
 
 
 def main():
     """encontrar perfectos hasta lim"""
     lim = 100_000
-    space_set = {0}  # hasta q nro habria q guardar?
+    space_set = {0}
+    start_time = time.time()
     for i in range(lim):
+        if (time.time() - start_time) > 10:
+            break
         sequence = [i]
         tries = 0
         holder = i
@@ -70,7 +37,7 @@ def main():
                     space_set.add(elem)
                 break
         if tries == 30:
-            print(f"30 reps for i == {i}")
+            print(f"30 iterations for i == {i}")
             for elem in sequence:
                 space_set.add(elem)
 
